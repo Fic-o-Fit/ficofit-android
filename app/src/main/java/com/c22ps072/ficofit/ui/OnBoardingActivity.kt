31180546622a913bdcafeb7f24551e970c698a32
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-05-19 14:29:05 +0700

    CAP-2 re-styling onboarding page #1

--- app/src/main/java/com/c22ps072/ficofit/ui/OnBoardingActivity.kt
+++ app/src/main/java/com/c22ps072/ficofit/ui/OnBoardingActivity.kt
@@ -1,11 +1,13 @@
 package com.c22ps072.ficofit.ui
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.c22ps072.ficofit.MainActivity
-import com.c22ps072.ficofit.R
 import com.c22ps072.ficofit.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
@@ -17,9 +19,23 @@ class OnBoardingActivity : AppCompatActivity() {
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupView()
+
         binding.fabIntent.setOnClickListener {
             Log.e("OnBoarding", "FAB clicked")
             startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
         }
     }
 }
